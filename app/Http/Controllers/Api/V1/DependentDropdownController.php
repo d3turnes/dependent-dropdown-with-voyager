91aa158cb5eef9b82a92e8c3d171c5b8217ddeb6
d3turnes <32837544+d3turnes@users.noreply.github.com>
2019-08-27 04:11:25 +0200

    Update DependentDropdownController.php

--- app/Http/Controllers/Api/V1/DependentDropdownController.php
+++ app/Http/Controllers/Api/V1/DependentDropdownController.php
@@ -5,17 +5,18 @@ namespace App\Http\Controllers\Api\V1;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class DependentDropdownController extends Controller
-{
-    public function index() {
+class DependentDropdownController extends Controller {
+	
+		public function index() {
 		
-		$params = request()->all();
+				$params = request()->all();
 		
-		$options = app($params['model'])->where($params['options']['where'], '=', $params['value'])->pluck($params['options']['label'], $params['options']['key']);
+				$options = app($params['model'])->where($params['options']['where'], '=', $params['value'])->pluck($params['options']['label'], $params['options']['key']);
 		
-		return response()->json([
-			'dropdown' => sprintf('#%s', $params['options']['name']),
-			'options' => $options
-		], 200);
-	}
+				return response()->json([
+					'dropdown' => sprintf('#%s', $params['options']['name']),
+					'options' => $options
+				], 200);
+	
+		}
 }
