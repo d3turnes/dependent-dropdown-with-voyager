13249954ededf91770ead1d89caee09f085ac6bc
David Turnes <d3turnes@gmail.com>
2019-08-27 04:05:17 +0200

    Create 2019_08_25_101607_create_profiles_table.php

---
+++ example2/database/migrations/2019_08_25_101607_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+			$table->unsignedInteger('user_id');
+			$table->unsignedInteger('city_id')->nullable();
+            $table->timestamps();
+			
+			/** foreign key */
+			$table->foreign('user_id')->references('id')->on('users');
+			/** foreign key */
+			$table->foreign('city_id')->references('id')->on('cities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
