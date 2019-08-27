93eb41a3bff9c5f2e1817d46e83908f733383d3d
David Turnes <d3turnes@gmail.com>
2019-08-27 04:05:25 +0200

    Create 2019_08_25_010726_create_states_table.php

---
+++ example2/database/migrations/2019_08_25_010726_create_states_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->string('name');
+            $table->unsignedInteger('country_id');
+
+			/** foreign key */
+			$table->foreign('country_id')->references('id')->on('countries');	
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('states');
+    }
+}
