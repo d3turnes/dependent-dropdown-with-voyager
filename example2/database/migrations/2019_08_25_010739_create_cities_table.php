06dba7b9ac023d72384ac21acfaca6260f0beb6c
David Turnes <d3turnes@gmail.com>
2019-08-27 04:05:21 +0200

    Create 2019_08_25_010739_create_cities_table.php

---
+++ example2/database/migrations/2019_08_25_010739_create_cities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id')->index();
+			$table->string('name');
+			$table->unsignedInteger('state_id');
+
+			/** foreign key */
+			$table->foreign('state_id')->references('id')->on('states');		
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
