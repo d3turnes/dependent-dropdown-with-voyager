e2808df99d3f4f191396e050dc495d5e7cce5c93
David Turnes <d3turnes@gmail.com>
2019-08-27 04:06:04 +0200

    Create SelectDependentDropdown.php

---
+++ app/FormFields/SelectDependentDropdown.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\FormFields;
+
+use TCG\Voyager\FormFields\AbstractHandler;
+
+class SelectDependentDropdown extends AbstractHandler
+{
+    protected $codename = 'select_dependent_dropdown';
+
+    public function createContent($row, $dataType, $dataTypeContent, $options)
+    {
+        /** resource/views/vendor/voyager/formfields/select_dependent_dropdown/select_dependent_dropdown.blade.php */
+		return view('voyager::formfields.select_dependent_dropdown.select_dependent_dropdown', [
+            'row' => $row,
+            'options' => $options,
+            'dataType' => $dataType,
+            'dataTypeContent' => $dataTypeContent
+        ]);
+    }
+}
