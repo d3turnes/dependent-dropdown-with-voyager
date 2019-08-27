974210bda84175539a47c31f42cac48886142530
David Turnes <d3turnes@gmail.com>
2019-08-27 04:05:28 +0200

    Create 2019_08_25_010707_create_countries_table.php

---
+++ example2/database/migrations/2019_08_25_010707_create_countries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->increments('id')->index();
+			$table->string('name', 166);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
