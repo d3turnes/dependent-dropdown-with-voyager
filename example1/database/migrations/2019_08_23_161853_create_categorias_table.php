41fa7284d424515c4fdd968dc3a2e49dd72541af
David Turnes <d3turnes@gmail.com>
2019-08-27 04:05:54 +0200

    Create 2019_08_23_161853_create_categorias_table.php

---
+++ example1/database/migrations/2019_08_23_161853_create_categorias_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->increments('id');
+			$table->string('name', 161);
+			$table->string('slug', 161)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorias');
+    }
+}
