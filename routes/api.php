39625b06f557a20549605a448378f20c000e06e8
David Turnes <d3turnes@gmail.com>
2019-08-27 04:04:03 +0200

    Create api.php

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group([
+	'prefix' => 'v1', 
+	'as' => 'api.v1.', 
+	'namespace' => 'Api\\V1\\'], function() {
+	Route::post('/dependent-dropdown', ['uses' => 'DependentDropdownController@index', 'as' => 'dropdown']);
+});
