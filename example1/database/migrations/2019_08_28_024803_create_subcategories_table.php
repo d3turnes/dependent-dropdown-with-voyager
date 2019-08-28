5eb4002416335bb5ab01fe2aeb2a422f93dd0fe3
David Turnes <d3turnes@gmail.com>
2019-08-28 05:40:27 +0200

    Create 2019_08_28_024803_create_subcategories_table.php

---
+++ example1/database/migrations/2019_08_28_024803_create_subcategories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubcategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subcategories', function (Blueprint $table) {
+            $table->increments('id');
+			$table->string('name', 161);
+			$table->string('slug', 161)->unique();
+			$table->unsignedInteger('family_id');
+			
+			/** foreign key */
+			$table->foreign('family_id')->references('id')->on('families');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subcategories');
+    }
+}
