8505eec4bf9e2ddeeccbd9a031616134241173c5
David Turnes <d3turnes@gmail.com>
2019-08-28 14:23:17 +0200

    Create Office.php

---
+++ example2/models/Office.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $fillable = [
+		'city_id'
+	];
+	
+	public $timestamps = false;
+	
+	public function city() {
+		return $this->belongsTo('App\City');
+	}
+	
+	/**
+	 *
+	 *	return [
+	 *		'city_id' => xxx,
+	 *		'state_id' => yyy
+	 *		'country_id' => zzz
+	 *	]
+	 *
+	 */
+	public static function cityIdRelationship($id) {
+		
+		return 
+			self::where('offices.id', '=', $id)
+				->select('offices.city_id', 'states.id as state_id', 'countries.id as country_id')
+					->join('cities', 'offices.city_id', '=', 'cities.id')
+						->join('states', 'cities.state_id', '=', 'states.id')
+							->join('countries', 'states.country_id', '=', 'countries.id')
+								->first();
+	}
+}
